e0c84c4cf89f9f642f29c5a1dea30720a7ffdfcc
Kaizer Charania <kaizer.charania@adara.com>
2020-04-29 12:51:50 -0700

    final framework update

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setuptools.setup(
     version="0.0.4",
     author="DOT",
     author_email="dot@adara.com",
-    description="'{{cookiecutter.repo_name}}' sub package using cookiecutter {{ cookiecutter.project_folder}}",
+    description="'{{cookiecutter.repo_name}}' sub package using cookiecutter {{ cookiecutter.repo_name}}",
     long_description="Sample",
     long_description_content_type="text/markdown",
     include_package_data=True,
     url='https://github.com/dot-at-adara/{{cookiecutter.repo_name}}',
-    packages=['cookiecutter_test_framework.{{cookiecutter.project_folder}}.{{cookiecutter.project_slug}}',
+    packages=['cookiecutter_test_framework.{{cookiecutter.project_folder}}.{{cookiecutter.project_dir}}',
               '{{cookiecutter.test_folder}}'],
 
     classifiers=[
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     entry_points={
         'console_scripts': [
-            '{{cookiecutter.repo_name}}={{ cookiecutter.project_folder}}.core.setup.command_line:main',
+            '{{cookiecutter.repo_name}}={{ cookiecutter.repo_name}}.core.setup.command_line:main',
         ],
     },
     keywords=(
